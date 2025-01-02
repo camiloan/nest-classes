dc8df4d9e6d5c8805fa449a90d7f1529acf09cb3
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-01-01 21:21:57 -0500

    Agregar soporte para validaciones y Mongoose en la gestión de Pokémon, incluyendo DTOs y configuración de Biome

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePokemonDto } from './dto/create-pokemon.dto';
-import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Injectable } from "@nestjs/common";
+import { CreatePokemonDto } from "./dto/create-pokemon.dto";
+import { UpdatePokemonDto } from "./dto/update-pokemon.dto";
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
-  }
+	create(createPokemonDto: CreatePokemonDto) {
+		createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+		return createPokemonDto;
+	}
 
-  findAll() {
-    return `This action returns all pokemon`;
-  }
+	findAll() {
+		return `This action returns all pokemon`;
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} pokemon`;
+	}
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
-  }
+	update(id: number, updatePokemonDto: UpdatePokemonDto) {
+		return `This action updates a #${id} pokemon`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
-  }
+	remove(id: number) {
+		return `This action removes a #${id} pokemon`;
+	}
 }
