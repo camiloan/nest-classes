39852c2a9b99d3ac54d53bc9a63be27cc64b038d
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-02 11:31:04 -0500

    Add JWT authentication: implement login functionality, update user entity, and configure JWT module

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -3,11 +3,32 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
 	controllers: [AuthController],
 	providers: [AuthService],
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [TypeOrmModule.forFeature([User]), PassportModule.register({ defaultStrategy: "jwt" }),
+	JwtModule.registerAsync({
+		imports: [],
+		inject: [],
+		useFactory: async (configService: ConfigService) => {
+			return {
+				secret: configService.get<string>("JWT_SECRET"),
+				signOptions: {
+					expiresIn: "2h"
+				}
+			}
+		}
+	})],
+	/* 	JwtModule.register({
+			secret: process.env.JWT_SECRET,
+			signOptions:{
+				expiresIn: "2h"
+			}
+		})], */
 	exports: [TypeOrmModule],
 })
-export class AuthModule {}
+export class AuthModule { }
--- 04-teslo-shop/src/auth/auth.service.ts
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
-import { CreateUserDto } from "./dto/create-user.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
 import * as bcrypt from "bcrypt";
+import { LoginUserDto, CreateUserDto } from "./dto";
 
 @Injectable()
 export class AuthService {
@@ -21,8 +21,35 @@ export class AuthService {
 				password: bcrypt.hashSync(password, 10),
 			});
 			await this.userRepository.save(user);
-			delete user.password;
-			return user;
+			const { password: omitted, ...result } = user;
+			return result;
+		} catch (error) {
+			this.handleDBError(error);
+		}
+	}
+
+	async login(loginUserDto: LoginUserDto) {
+		try {
+			const user = await this.userRepository.findOne({
+				where: {
+					email: loginUserDto.email,
+				},
+				select: ["email", "password"],
+			});
+			if (!user) {
+				throw new BadRequestException("Credentials are not valid (email)");
+			}
+			const password = await bcrypt.compare(
+				loginUserDto.password,
+				user.password,
+			);
+			if (!password) {
+				throw new BadRequestException("Credentials are not valid (password)");
+			}
+			const { password: omitted, ...result } = user;
+			return result;
+
+			//TODO Return new jwt token
 		} catch (error) {
 			this.handleDBError(error);
 		}
