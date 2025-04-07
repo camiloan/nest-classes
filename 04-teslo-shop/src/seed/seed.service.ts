e5c091ef2f206592031480cb83b79af24b1352d6
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-07 13:46:44 -0500

    feat(seed): implement user seeding and refactor product insertion logic

--- 04-teslo-shop/src/seed/seed.service.ts
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -1,16 +1,49 @@
 import { Injectable } from "@nestjs/common";
 import { ProductsService } from "../products/products.service";
 import { initialData } from "../seed/data/seed-data";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/auth/entities/user.entity";
+import { Repository } from "typeorm";
 @Injectable()
 export class SeedService {
-	constructor(private readonly productsService: ProductsService) {}
+	constructor(private readonly productsService: ProductsService,
+		@InjectRepository(User)
+		private readonly userRepository: Repository<User>,
+
+	) { }
 
 	async runSeed() {
-		await this.insertNewProducts();
+		await this.deleteTables();
+		const adminUser = await this.insertUsers();
+		await this.insertNewProducts(adminUser);
 		return "SEED EXECUTED";
 	}
 
-	private async insertNewProducts() {
+	private async deleteTables() {
+		await this.productsService.deleteAllProducts();
+
+		const queryBuilder = this.userRepository.createQueryBuilder();
+		await queryBuilder.delete().where({}).execute();
+
+	}
+
+	private async insertUsers() {
+        const seedUsers = initialData.users; // Datos crudos
+        const users: User[] = []; // Array para instancias de entidad
+
+        // biome-ignore lint/complexity/noForEach: <explanation>
+        seedUsers.forEach(user => {
+            // Crea la instancia de la entidad User a partir del objeto plano
+            users.push(this.userRepository.create(user)); 
+        });
+
+        // Guarda las instancias de entidad que fueron creadas
+        const dbUsers = await this.userRepository.save(users); // <--- Usar 'users' aquí
+
+        return dbUsers[0]; 
+    }
+
+	private async insertNewProducts(user: User) {
 		this.productsService.deleteAllProducts();
 
 		const products = initialData.products;
@@ -19,7 +52,7 @@ export class SeedService {
 
 		for (let i = 0; i < products.length; i++) {
 			const product = products[i];
-			insertPromises.push(this.productsService.create(product));
+			insertPromises.push(this.productsService.create(product, user));
 		}
 
 		await Promise.all(insertPromises);
