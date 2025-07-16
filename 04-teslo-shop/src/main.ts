54369fcf7c47622797bc883ad2f623a26443f2c9
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-07-15 23:08:24 -0500

    chore: update dependencies and add Swagger documentation
    - Updated NestJS dependencies to version 11.1.3
    - Updated bcrypt to version 6.0.0
    - Updated class-validator to version 0.14.2
    - Updated pg to version 8.16.3
    - Updated typeorm to version 0.3.25
    - Updated devDependencies including eslint, jest, and prettier
    - Added Swagger documentation setup in main.ts

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
 			forbidNonWhitelisted: true,
 		}),
 	);
+
+
+	const config = new DocumentBuilder()
+		.setTitle('Testlo RESTful API')
+		.setDescription('Teslo shop endpoints')
+		.setVersion('1.0')
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
+
 	await app.listen(process.env.PORT ?? 3000);
 	logger.log(`Application is running on: ${await app.getUrl()}`);
 }
